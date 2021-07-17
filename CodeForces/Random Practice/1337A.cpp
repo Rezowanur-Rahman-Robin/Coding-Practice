695f002457643d4e55f9173240cf810cff8d6ddc
Ratul1997 <robincuetcse@gmail.com>
2021-07-17 23:18:22 +0600

    5 new problem solved at 17-07-2021

---
+++ CodeForces/Random Practice/1337A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    long long int a,b,c,d;
+    cin>>t;
+    while(t--)
+    {
+        cin>>a>>b>>c>>d;
+        cout<<b<<" "<<c<<" "<<c<<endl;
+    }
+    return 0;
+}
