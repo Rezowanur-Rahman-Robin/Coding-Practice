3e81fea769aa0e22a96200d229511e5baa609d0b
Ratul1997 <robincuetcse@gmail.com>
2021-08-06 21:19:13 +0600

    3 cf 06-08-2021

---
+++ CodeForces/Random Practice/1399C.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+//	freopen("output.txt", "w", stdout);
+#endif
+
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		vector<int> cnt(n + 1);
+		for (int i = 0; i < n; ++i) {
+			int x;
+			cin >> x;
+			++cnt[x];
+		}
+		int ans = 0;
+		for (int s = 2; s <= 2 * n; ++s) {
+			int cur = 0;
+			for (int i = 1; i < (s + 1) / 2; ++i) {
+				if (s - i > n) continue;
+				cur += min(cnt[i], cnt[s - i]);
+			}
+			if (s % 2 == 0) cur += cnt[s / 2] / 2;
+			ans = max(ans, cur);
+		}
+		cout << ans << endl;
+	}
+
+	return 0;
+}
