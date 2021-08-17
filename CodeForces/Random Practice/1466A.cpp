1ccd562ea398d879229ff2e1f116471f765558e0
Ratul1997 <robincuetcse@gmail.com>
2021-08-17 23:36:13 +0600

    4 cf 17-08-2021

--- CodeForces/Random Practice/1466A.cpp
+++ CodeForces/Random Practice/1466A.cpp
@@ -1,32 +1,29 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include <set>
+#include <cmath>
 
 using namespace std;
-
-void solve() {
-	int n;
-	scanf("%d", &n);
-
-	vector <int> in(n);
-	vector <int> is(n);
-
-	for(auto &p: in)
-		scanf("%d", &p);
-
-	for(int i = 0; i < n; ++i)
-		for(int j = i + 1; j < n; ++j)
-			is[in[j] - in[i]] = true;
-
-	int ans = 0;
-	for(auto &v: is)
-		ans += v;
-	printf("%d\n", ans);
-}
-
-int main() {
-	int cases;
-	scanf("%d", &cases);
-
-	while(cases--)
-		solve();
-	return 0;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    for (int i = 0; i < t; ++i) {
+        int n;
+        cin>>n;
+        float ar[n];
+        for (int j = 0; j < n; ++j) {
+            cin>>ar[j];
+        }
+        set<float> st;
+        for (int j = 0; j < n; ++j) {
+            for (int k = j+1; k < n; ++k) {
+                float areaSquare = abs(ar[k]-ar[j]);
+                if (areaSquare!=0) {
+                    st.insert(areaSquare);
+                }
+            }
+        }
+        cout<<st.size()<<"\n";
+    }
 }
