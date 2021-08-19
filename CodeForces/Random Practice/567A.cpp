8f43f6fd88c3f36b86cafcad477326fdb2436ae8
Ratul1997 <robincuetcse@gmail.com>
2021-08-20 00:38:24 +0600

    2 cf 20-08-2021

---
+++ CodeForces/Random Practice/567A.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	 int n;
+	 cin>>n;
+	 int arr[n];
+	 for(int i=0;i<n;i++){
+		 cin>>arr[i];
+	 }
+	 for(int i=0;i<n;i++){
+		 long long  mini;
+		 long long maxi;
+		 if(!i){
+			mini=abs(arr[i]-arr[i+1]);
+			maxi=abs(arr[i]-arr[n-1]);
+		 }
+		 else if(i==n-1){
+			 maxi=abs(arr[i]-arr[0]);
+			 mini=abs(arr[i]-arr[i-1]);
+		 }
+		 else{
+			 mini=min(abs(arr[i]-arr[i+1]),abs(arr[i]-arr[i-1]));
+			 maxi=max(abs(arr[i]-arr[0]),abs(arr[i]-arr[n-1]));
+		 }
+		 cout<<mini<<" "<<maxi<<"\n";
+	 }
+	return 0;
+}
