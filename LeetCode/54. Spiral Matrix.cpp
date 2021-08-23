fef63116112607a9a03efc3803fe48fb6d61a4c5
Ratul1997 <robincuetcse@gmail.com>
2021-08-24 05:36:00 +0600

    2 leetcode 24-08-2021

---
+++ LeetCode/54. Spiral Matrix.cpp
@@ -0,0 +1,98 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int counting = 0;
+
+        int rowStart = 0 , rowEnd = m-1 , colStart = 0 ,colEnd = n-1;
+
+        vector<int> spiralMatrix;
+
+        if(matrix.empty()){
+            return spiralMatrix;
+        }
+        while(rowStart <= rowEnd && colStart <= colEnd){
+            ///print starting row
+            for(int i = colStart ; i<=colEnd ; i++){
+                spiralMatrix.push_back(matrix[rowStart][i]);
+                counting++;
+            }
+            rowStart++;
+
+            if(counting == m*n )
+                return spiralMatrix;
+
+             ///print End Column
+              for(int i = rowStart ; i<=rowEnd ; i++){
+                spiralMatrix.push_back(matrix[i][colEnd]);
+                counting++;
+            }
+            colEnd--;
+
+            if(counting == m*n )
+                return spiralMatrix;
+
+            ///print ending row
+            for(int i = colEnd ; i>=colStart ; i--){
+                spiralMatrix.push_back(matrix[rowEnd][i]);
+                counting++;
+            }
+            rowEnd--;
+
+            if(counting == m*n )
+                return spiralMatrix;
+
+
+            ///print Start Column
+              for(int i = rowEnd ; i>=rowStart ; i--){
+                spiralMatrix.push_back(matrix[i][colStart]);
+                counting++;
+            }
+            colStart++;
+
+            if(counting == m*n )
+                return spiralMatrix;
+
+        }
+
+
+
+        return spiralMatrix;
+
+    }
+};
+
+int main(){
+
+int m;
+int n;
+cin>>m;
+cin>>n;
+vector<vector<int>> matrix(m);
+int temp;
+
+for(int i=0;i<m;i++){
+    for(int j=0; j< n ; j++){
+        cin>>temp;
+        matrix[i].push_back(temp);
+    }
+}
+
+Solution s;
+
+vector<int> spiralMatrix;
+
+spiralMatrix = s.spiralOrder(matrix);
+
+        for(int i=0;i<spiralMatrix.size();i++){
+        cout<<spiralMatrix[i]<<" ";
+}
+
+
+return 0;
+}
---
+++ LeetCode/59. Spiral Matrix II.cpp
@@ -0,0 +1,91 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> spiralOrder(int n) {
+
+
+        int counting = 0;
+
+        int rowStart = 0 , rowEnd = n-1 , colStart = 0 ,colEnd = n-1;
+
+        vector<vector<int>> spiralMatrix(n,vector<int> (n, 0));
+
+        if(n==0){
+            return spiralMatrix;
+        }
+        while(rowStart <= rowEnd && colStart <= colEnd){
+            ///input starting row
+            for(int i = colStart ; i<=colEnd ; i++){
+                spiralMatrix[rowStart][i]=(counting + 1);
+                counting++;
+            }
+            rowStart++;
+
+            if(counting == n*n )
+                return spiralMatrix;
+
+             ///input End Column
+              for(int i = rowStart ; i<=rowEnd ; i++){
+                spiralMatrix[i][colEnd] = (counting + 1);
+                counting++;
+            }
+            colEnd--;
+
+            if(counting == n*n )
+                return spiralMatrix;
+
+            ///input ending row
+            for(int i = colEnd ; i>=colStart ; i--){
+                spiralMatrix[rowEnd][i] = (counting + 1);
+                counting++;
+            }
+            rowEnd--;
+
+            if(counting == n*n )
+                return spiralMatrix;
+
+
+            ///input Start Column
+              for(int i = rowEnd ; i>=rowStart ; i--){
+                spiralMatrix[i][colStart] = (counting + 1 );
+                counting++;
+            }
+            colStart++;
+
+            if(counting == n*n )
+                return spiralMatrix;
+
+        }
+
+
+
+        return spiralMatrix;
+
+    }
+};
+
+int main(){
+
+int N;
+cin>>N;
+
+
+Solution s;
+
+vector<vector<int>> spiralMatrix;
+
+spiralMatrix = s.spiralOrder(N);
+
+        for(int i=0;i<spiralMatrix.size();i++){
+          for(int j=0;j<spiralMatrix[0].size();j++){
+            cout<<spiralMatrix[i][j]<<" ";
+          }
+         cout<<endl;
+        }
+
+
+return 0;
+}
